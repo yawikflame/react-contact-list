376ff5ba6f404fadfebb9229f73fa2d6cd0ffd64
Yaron Lavi <yawikflame@gmail.com>
2020-07-08 15:33:31 -0700

    Updates

---
+++ precache-manifest.f93dd575418928f37c2ca04e2e113a08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001d56512aaaea9ee0dd761a0895ad66",
+    "url": "/react-contact-list/index.html"
+  },
+  {
+    "revision": "1a8127ef5ef88baf5c42",
+    "url": "/react-contact-list/static/js/2.88d657d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-contact-list/static/js/2.88d657d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee274d536400064d90bf",
+    "url": "/react-contact-list/static/js/main.6f9a5494.chunk.js"
+  },
+  {
+    "revision": "ae7678212b1f1004d5e9",
+    "url": "/react-contact-list/static/js/runtime-main.8cbf87cf.js"
+  }
+]);
